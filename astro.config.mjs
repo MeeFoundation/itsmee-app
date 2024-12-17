7d15370b2834525e309f1baf0b54517aef6a4527
Raman Siliuk <rsiluk@swiftinvention.com>
2024-12-17 16:02:53 +0400

    build fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-export const site = "https://itsmee.org";
+export const site = "https://app.itsmee.org";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   integrations: [tailwind()],
-  adapter: node({
-    mode: "standalone",
-  }),
   site: site,
 });
